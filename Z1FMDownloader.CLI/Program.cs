3ca22edc0672a7030b1382995aa43c0f02d9c26e
nazarnik050406 <nazarnik050406@gmail.com>
2021-01-24 12:33:29 +0200

    Initialized console tool repository

---
+++ Z1FMDownloader.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Z1FMDownloader.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
